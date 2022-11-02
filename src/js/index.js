21ec8b0c63575589636896245cd2d32259f2fab7
Alex <needdd3@gmail.com>
2022-11-01 22:13:00 -0400

    feat: intro section, cards section, header-hovers

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@
 const menuBtn = document.querySelector('.menu-icon');
 const menu = document.querySelector('.nav-menu');
 const body = document.querySelector('body');
+
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('_active');
     menuBtn.classList.toggle('_active');
